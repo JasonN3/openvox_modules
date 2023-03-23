19414a4f9c0190d726e75e57b93ec6b93b9364bb
JasonN3 <33561705+JasonN3@users.noreply.github.com>
2023-03-23 14:16:58 -0500

    add unit tests

---
+++ domain_join/spec/classes/domain_join_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rspec-puppet'
+
+describe 'domain_join' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) {
+        {
+          'username' => "join_user",
+          'sensitive_password' => RSpec::Puppet::Sensitive.new("test"),
+          'global_admins' => "EXAMPLE Linux Admins",
+          'global_ssh' => "EXAMPLE Linux SSH Users",
+          'local_admins' => "EXAMPLE %HOSTNAME% Admins",
+          'local_ssh' => "EXAMPLE %HOSTNAME% SSH Users",
+          'file_header' => 'Puppet managed'
+        }
+      }
+
+      it { is_expected.to compile }
+    end
+  end
+end
