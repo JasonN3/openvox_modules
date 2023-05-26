8d67ad8bcd6c0048a7d7e9f4ae14577fb6997b57
Jason N <33561705+JasonN3@users.noreply.github.com>
2023-05-26 02:46:15 +0000

    add support for Rocky

--- hashicorp_vault/manifests/lib_binary.pp
+++ hashicorp_vault/manifests/lib_binary.pp
@@ -6,7 +6,7 @@ class hashicorp_vault::lib_binary {
   case $facts['os']['family'] {
     'RedHat': {
       case $facts['os']['name'] {
-        /RedHat|CentOS/: {
+        /RedHat|CentOS|Rocky/: {
           file { '/etc/yum.repos.d/vault.repo':
             source => 'https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo',
             owner  => root,
