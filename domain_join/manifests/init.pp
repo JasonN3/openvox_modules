996135ef98d413d0e90888274262aad435e4302d
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2026-02-11 05:04:27 +0000

    add validation

--- domain_join/manifests/init.pp
+++ domain_join/manifests/init.pp
@@ -77,6 +77,24 @@ class domain_join (
   Optional[Sensitive[String]]                                $client_secret        = undef,
   Optional[String]                                           $tenant_id            = undef
 ) {
+  if $oidc {
+    if !$client_id {
+      fail('domain_join: client_id must be provided when oidc is enabled')
+    }
+    if !$client_secret {
+      fail('domain_join: client_secret must be provided when oidc is enabled')
+    }
+    if !$tenant_id {
+      fail('domain_join: tenant_id must be provided when oidc is enabled')
+    }
+  } else {
+    if !username {
+      fail('domain_join: username must be provided when oidc is disbled')
+    }
+    if !sensitive_password {
+      fail('domain_join: sensitive_password must be provided when oidc is disbled')
+    }
+  }
   if $override_domain {
     $currdomain = $override_domain
     # This is only used if $override_domain is defined
